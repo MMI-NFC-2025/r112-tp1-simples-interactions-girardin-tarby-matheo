ed2c33651700100040bd6638ca6ff8d9b49a8664
girardin-tarby-matheo <matheo.girardin--tarby@edu.univ-fcomte.fr>
2025-11-07 14:32:29 +0100

    collection d'éléments

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,10 @@ const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
 enRougeSuiteAClick.addEventListener("click", (evt)=>{
     enRougeSuiteAClick.style.color = "red"
 });
+
+const collH2 = document.querySelectorAll("h2")
+collH2.forEach((elem)=>{
+    elem.addEventListener("click", (evt)=>{
+        evt.target.style.color = "red";
+    });    
+});
